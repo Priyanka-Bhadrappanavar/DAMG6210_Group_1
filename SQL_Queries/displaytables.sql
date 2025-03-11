0af98beb28cbeac1416400692817392b1ba55bea
sujaldusane <dusane.s@northeastern.edu>
2025-03-10 23:02:26 -0400

    SQL files added

---
+++ SQL_Queries/displaytables.sql
@@ -0,0 +1,19 @@
+USE FINAL_HOS_MNG;
+GO
+
+SELECT * FROM Department;
+SELECT * FROM Specialization;
+SELECT * FROM Doctor;
+SELECT * FROM Doctor_Specialization;
+SELECT * FROM Admin;
+SELECT * FROM Patient;
+SELECT * FROM Emergency_Case;
+SELECT * FROM Medical_Record;
+SELECT * FROM Diagnosis;
+SELECT * FROM Prescription;
+SELECT * FROM Schedule;
+SELECT * FROM Appointment;
+SELECT * FROM Notification;
+SELECT * FROM PaymentMethod;
+SELECT * FROM Payment;
+SELECT * FROM Payment_PaymentMethod;
